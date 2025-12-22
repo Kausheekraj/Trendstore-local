292c9ac283865698af5dd054d912d41773ff02ab
Kausheekraj <kausheek94@gmail.com>
2025-12-22 13:56:19 +0530

    New push

--- operation/infra/terraform/main.tf
+++ operation/infra/terraform/main.tf
@@ -1,34 +1,6 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-
-
-data "aws_ami" "amazon_linux_2" {
-  owners      = ["amazon"]
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "state"
-    values = ["available"]
-  }
-}
-
 # VPC
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
@@ -36,182 +8,134 @@ resource "aws_vpc" "main" {
     Name = "trendstore-vpc"
   }
 }
-
-# Public subnet
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "${var.aws_region}a"
-
-  tags = {
-    Name = "trendstore-public-subnet"
-  }
-}
-
-# Internet gateway
+# Internet-gateway
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.eks_vpc.id
 
   tags = {
     Name = "trendstore-igw"
   }
 }
+# subnet
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
 
-# Route table
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+  tags = {
+    Name                     = "trendstore-public-${count.index}"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name                              = "trendstore-private-${count.index}"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+
+# route table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.eks_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-
-  tags = {
-    Name = "trendstore-public-rt"
-  }
 }
 
 resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+# EKS Cluster
+resource "aws_eks_cluster" "eks" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = concat(
+      aws_subnet.public[*].id,
+      aws_subnet.private[*].id
+    )
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
+}
+# node group
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "trendstore-nodes"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = aws_subnet.private[*].id
+
+  scaling_config {
+    desired_size = var.desired_nodes
+    min_size     = var.min_nodes
+    max_size     = var.max_nodes
+  }
+
+  instance_types = [var.node_instance_type]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.worker_node_policies
+  ]
 }
 
-# Security group for Jenkins EC2
-resource "aws_security_group" "jenkins_sg" {
-  name        = "jenkins-sg"
-  description = "Allow SSH and Jenkins"
-  vpc_id      = aws_vpc.main.id
-
-  # SSH
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Jenkins UI
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Optional: NodePort / app debug port
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "jenkins-sg"
-  }
-}
-
-# IAM role so EC2 can talk to EKS / CloudWatch / ECR etc.
-resource "aws_iam_role" "jenkins_role" {
-  name = "jenkins-ec2-role"
+# iam roles
+# Custer roles
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eksClusterRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
+      Effect    = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+      Action    = "sts:AssumeRole"
     }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "jenkins_AmazonEKSClusterPolicy" {
-  role       = aws_iam_role.jenkins_role.name
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "jenkins_AmazonEKSWorkerNodePolicy" {
-  role       = aws_iam_role.jenkins_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+# worker node roles
+resource "aws_iam_role" "eks_node_role" {
+  name = "eksNodeRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "jenkins_CloudWatchAgentServerPolicy" {
-  role       = aws_iam_role.jenkins_role.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+resource "aws_iam_role_policy_attachment" "worker_node_policies" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  ])
+
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = each.value
 }
-
-resource "aws_iam_instance_profile" "jenkins_profile" {
-  name = "jenkins-ec2-profile"
-  role = aws_iam_role.jenkins_role.name
-}
-
-# Jenkins EC2 instance
-resource "aws_instance" "jenkins" {
-  ami                         = data.aws_ami.amazon_linux_2.id
-  instance_type               = "t3.small"
-  subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.jenkins_sg.id]
-  key_name                    = var.key_name
-  iam_instance_profile        = aws_iam_instance_profile.jenkins_profile.name
-  associate_public_ip_address = true
-
-  root_block_device {
-    volume_size = 30
-    volume_type = "gp3"
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              # Install Docker
-              amazon-linux-extras install docker -y
-              systemctl enable docker
-              systemctl start docker
-              usermod -aG docker ec2-user
-
-              # Install git and Java for Jenkins
-              yum install -y git java-11-amazon-corretto
-                
-              yum install -y curl gnupg software-properties-common
-
-              curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null && \
-              echo 'deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/' > /etc/apt/sources.list.d/jenkins.list
-                yum update
-
-            yum install -y openjdk-17-jdk jenkins
-          
-              # Install awscli v2 and kubectl
-              yum install -y unzip
-              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
-              unzip /tmp/awscliv2.zip -d /tmp
-              /tmp/aws/install
-
-              curl -o /usr/local/bin/kubectl \
-                https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-03-11/bin/linux/amd64/kubectl
-              chmod +x /usr/local/bin/kubectl
-
-              EOF
-
-  tags = {
-    Name = "jenkins-ec2"
-  }
-}
-
-output "jenkins_public_ip" {
-  value = aws_instance.jenkins.public_ip
-}
-
-output "jenkins_url" {
-  value = "http://${aws_instance.jenkins.public_ip}:8080"
+data "aws_availability_zones" "available" {
+  state = "available"
 }
 
--- operation/infra/terraform/var.tf
+++ operation/infra/terraform/var.tf
@@ -1,10 +1,45 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-2"
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
 }
 
-variable "key_name" {
+variable "cluster_name" {
+  description = "EKS cluster name"
   type        = string
-  description = "Name of an existing EC2 key pair in this region"
+  default     = "trendstore-eks"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  type    = list(string)
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "node_instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "desired_nodes" {
+  default = 2
+}
+
+variable "min_nodes" {
+  default = 1
+}
+
+variable "max_nodes" {
+  default = 5
 }
 
